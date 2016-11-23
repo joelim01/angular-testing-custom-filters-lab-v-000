5d2ddb3039ccf9f897a93d8ffa20934f7f51195f
Joseph Lim <joseph.d.lim@gmail.com>
2016-11-23 14:58:22 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,16 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood, userList;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+			favoriteFood = $filter('favoriteFood');
 	}));
 
-	
+	userList = [{name: "Ethan", favoriteFood: "pizza"}, {name: "Jack", favoriteFood: "Buffalo wings"}]
+
+	it('should find matching users', function () {
+			expect(favoriteFood(userList, "pizza")).toEqual([{name: "Ethan", favoriteFood: "pizza"}]);
+	});
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,13 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+			removeAllVowels = $filter('removeAllVowels');
 	}));
 
+	it('should not return vowels', function () {
+			expect(removeAllVowels("pizza")).toEqual("pzz");
+	});
 });
